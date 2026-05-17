40f8d3b79aeac96312108d12a09b80dbf942650f
shashika011 <shashikaus@gmaail.com>
2026-05-17 18:52:43 +0530

    my commit n

---
+++ src/test/java/com/swaglabtest/pages/test.java
@@ -0,0 +1,11 @@
+package com.swaglabtest.pages;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a test class");
+
+	}
+
+}
