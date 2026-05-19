cb9b0bc346579de290a099c355ecf1efc9e9377d
shashika011 <shashikaus@gmaail.com>
2026-05-20 02:04:30 +0530

    add more changes on this branch1

--- src/test/java/com/swaglabtest/pages/testclass.java
+++ src/test/java/com/swaglabtest/pages/testclass.java
@@ -7,8 +7,7 @@ public class testclass {
         System.out.println("Hello World1");
         System.out.println("Hello World2");
         System.out.println("Hello World3");
-        System.out.println("Hello World4");
-        System.out.println("Hello World5");
+     
 	}
 
 }
