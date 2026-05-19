79cef4b5708065e066a04b7873ab6afd3eaaa132
shashika011 <shashikaus@gmaail.com>
2026-05-20 01:22:10 +0530

    new changes more

--- src/test/java/com/swaglabtest/pages/testclass.java
+++ src/test/java/com/swaglabtest/pages/testclass.java
@@ -5,6 +5,7 @@ public class testclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("Hello World1");
+        System.out.println("Hello World2");
 	}
 
 }
